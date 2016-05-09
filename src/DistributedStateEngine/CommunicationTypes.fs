5891f7511fbcaa3ebf5ba086e16a1e6a158d3ccf
danielr <danielr@digiterre.com>
2016-05-09 12:04:17 +0100

    Add types used for RPC

---
+++ src/DistributedStateEngine/CommunicationTypes.fs
@@ -0,0 +1,14 @@
+﻿module internal CommunicationTypes
+    open System
+
+    type AppendEntriesOut = { term:uint64; leaderId:string; prevLogIndex:uint64; prevLogTerm:uint64; entries:string[]; leaderCommit:uint64 }
+
+    type AppendEntriesIn = { term:uint64; success:bool }
+
+    type RequestVoteOut = { term:uint64; candidateId:string; lastLogIndex:uint64; lastLogTerm:uint64; }
+
+    type RequestVoteIn = { term:uint64; voteGranted:bool }
+
+    type RpcResponse =
+        | AppendEntries of AppendEntriesIn
+        | RequestVote of RequestVoteIn
