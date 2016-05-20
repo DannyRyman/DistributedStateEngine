85db86c0cebc749cd45ffad3abc02fe90e4b752f
Dan Ryan <mail.dan.ryan@googlemail.com>
2016-05-20 11:09:10 +0100

    Breakup Raft file with comments

--- src/DistributedStateEngine/RaftWorkflow.fs
+++ src/DistributedStateEngine/RaftWorkflow.fs
@@ -4,6 +4,9 @@ open Serilog
 open System
 open System.Threading
 
+//---------------------------------------------------------------------------
+// Types
+//---------------------------------------------------------------------------
 type State = 
   | Follower
   | Candidate
@@ -26,52 +29,58 @@ type RaftEvent =
 type ServerEvents =
   | ServerStarted
         
+//---------------------------------------------------------------------------
+// Main raft workflow
+//---------------------------------------------------------------------------
 let processRaftEvent (initialContext:Context,raftEvent:RaftEvent) = 
   match raftEvent with
   | ElectionTimeout -> 
     { initialContext with State = Candidate }
     
+//---------------------------------------------------------------------------
+// Raft server
+//---------------------------------------------------------------------------
 type Server(electionTimeoutService : ITimeoutService, 
               heartbeatTimeoutService : ITimeoutService,
               raftWorkflow:(Context*RaftEvent)->Context,
               loggerConfig:LoggerConfiguration) =         
-    do
-      Log.Logger <- loggerConfig.CreateLogger()
+  do
+    Log.Logger <- loggerConfig.CreateLogger()
 
-    let serverEvent = new Event<ServerEvents>()
+  let serverEvent = new Event<ServerEvents>()
 
-    let mutable context:Option<Context> = None
+  let mutable context:Option<Context> = None
 
-    let workflowProcessor = MailboxProcessor<RaftEvent>.Start(fun mailbox -> 
-      async {
-        let! event = mailbox.Receive()
-        Log.Information("Received event {event}", event)        
-        let newContext = raftWorkflow (context.Value, event)
-        Log.Information("Finished processing {event}; Original context {context}; New Context {newContext}", sprintf "%A" event, sprintf "%A" context.Value, sprintf "%A" newContext)
-        context <- Some newContext
-      }      
-    )
+  let workflowProcessor = MailboxProcessor<RaftEvent>.Start(fun mailbox -> 
+    async {
+      let! event = mailbox.Receive()
+      Log.Information("Received event {event}", event)        
+      let newContext = raftWorkflow (context.Value, event)
+      Log.Information("Finished processing {event}; Original context {context}; New Context {newContext}", sprintf "%A" event, sprintf "%A" context.Value, sprintf "%A" newContext)
+      context <- Some newContext
+    }      
+  )
 
-    let initializeContext () =
-      // todo load from persistance if necessary
-      Some {State=Follower;CurrentTerm=0UL;PreviousLogIndexes=None}
+  let initializeContext () =
+    // todo load from persistance if necessary
+    Some {State=Follower;CurrentTerm=0UL;PreviousLogIndexes=None}
 
-    member this.ServerEvent = serverEvent.Publish
+  member this.ServerEvent = serverEvent.Publish
 
-    member this.Start (cancellationToken : CancellationToken) = 
-      async {
-        context <- initializeContext ()
-        workflowProcessor.Error.Add(fun ex -> Log.Error("Error {ex}", ex))
-        use electionTimeoutServiceSubscription = electionTimeoutService.TimedOut.Subscribe(fun () -> workflowProcessor.Post(ElectionTimeout))      
-        electionTimeoutService.Start()
-        serverEvent.Trigger(ServerStarted)
-        cancellationToken.WaitHandle.WaitOne() |> ignore    
-      }        
+  member this.Start (cancellationToken : CancellationToken) = 
+    async {
+      context <- initializeContext ()
+      workflowProcessor.Error.Add(fun ex -> Log.Error("Error {ex}", ex))
+      use electionTimeoutServiceSubscription = electionTimeoutService.TimedOut.Subscribe(fun () -> workflowProcessor.Post(ElectionTimeout))      
+      electionTimeoutService.Start()
+      serverEvent.Trigger(ServerStarted)
+      cancellationToken.WaitHandle.WaitOne() |> ignore    
+    }        
 
-    member this.GetContext () =
-      match context with
-      | Some c -> c
-      | None -> failwith "Cannot get context as server is not started"    
+  member this.GetContext () =
+    match context with
+    | Some c -> c
+    | None -> failwith "Cannot get context as server is not started"    
       
         
 
